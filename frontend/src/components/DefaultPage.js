218172ad3e31e7dd0a1ea951240a53ff205c02d4
haripajjuri <20mh1a4241@acoe.edu.in>
2023-12-30 15:24:58 +0530

    updated styles

--- frontend/src/components/DefaultPage.js
+++ frontend/src/components/DefaultPage.js
@@ -1,7 +1,9 @@
+import img from '../logo.png' 
 export default function DefaultPage(){
     return(
-        <div className="h-screen  border-2 border-red-300">
-
+        <div className="h-screen flex justify-center items-center opacity-50 flex-col">
+            <p className='font-semibold text-todo-textPrimary'>BROWSE  YOUR  PROJECTS</p>
+            <img className='w-2/5' src={img} alt="" />
         </div>
     )
 }
