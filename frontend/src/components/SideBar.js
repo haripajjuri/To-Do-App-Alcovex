c6030ddbc625927b7f6cc19b48145652923ba8d3
haripajjuri <20mh1a4241@acoe.edu.in>
2024-01-20 23:04:05 +0530

    added loading animation

--- frontend/src/components/SideBar.js
+++ frontend/src/components/SideBar.js
@@ -4,21 +4,24 @@ import ProjectCard from "./ProjectCard";
 import ReactModal from "react-modal";
 import CreateProject from "./CreateProject";
 import { useNavigate } from "react-router-dom";
+import MoonLoader from 'react-spinners/MoonLoader';
 
 export default function SideBar(){
     const navigate = useNavigate();
     const [projects,setProjects] = useState([]);
+    const [loading,setLoading] = useState(true);
     const [visible,setVisible] = useState(false);
 
     useEffect(()=>{
         getData();
     },[])
 
-    function getData(){
-        axios.get(`${process.env.REACT_APP_URL}/projects`).then(res=>{
+    async function getData(){
+        await axios.get(`${process.env.REACT_APP_URL}/projects`).then(res=>{
             setProjects(res.data);
         }
         )
+        setLoading(false);
     }
 
     function changeState(val){
@@ -36,6 +39,7 @@ export default function SideBar(){
             task Boards
             </div>
             {
+                loading?<MoonLoader color="#0c9dff" size={35}/>:
                 projects?
                 <>
                 <div className="w-10/12 flex flex-col gap-1.45 py-3">
@@ -50,16 +54,17 @@ export default function SideBar(){
                 <>
                 <div>no projects available</div>
                 </>
+            
             }
-
             <div className="my-3 p-2.5 rounded-lg text-m flex items-center gap-3 cursor-pointer"  onClick={()=>setVisible(true)} >
             <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="14" height="14" viewBox="0 0 24 24">
             <path fill-rule="evenodd" d="M 11 2 L 11 11 L 2 11 L 2 13 L 11 13 L 11 22 L 13 22 L 13 13 L 22 13 L 22 11 L 13 11 L 13 2 Z"></path>
             </svg>
             <button className="text-blue-500">Add New Project</button>
             </div>
-            </div>
 
+            </div>
+        
             <ReactModal isOpen={visible} className="h-screen">
                 <CreateProject changeState={changeState} />
             </ReactModal>
